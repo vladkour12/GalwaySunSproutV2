9e9ffe10bb03a74f39c0046aba427dba0ab7abfb
vladkour12 <vladkour1@gmail.com>
2025-12-15 03:30:04 +0000

    Add assetsInclude for JSON files in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      assetsInclude: ['**/*.json']
     };
 });
